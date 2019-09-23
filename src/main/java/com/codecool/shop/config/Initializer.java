02b6998d1c3d0907515e6dfece8fa108577b2ef3
daniel-kalocsay <kdani782@gmail.com>
2019-09-23 14:48:48 +0200

    added new products and categories

--- src/main/java/com/codecool/shop/config/Initializer.java
+++ src/main/java/com/codecool/shop/config/Initializer.java
@@ -28,19 +28,33 @@ public class Initializer implements ServletContextListener {
         supplierDataStore.add(amazon);
         Supplier lenovo = new Supplier("Lenovo", "Computers");
         supplierDataStore.add(lenovo);
+        Supplier apple = new Supplier("Apple", "All kinds of useless overpriced hardware");
+        supplierDataStore.add(apple);
+        Supplier samsung = new Supplier("Samsung", "All kinds of actually useful hardware. Still overpriced");
+        supplierDataStore.add(samsung);
+        Supplier tesla = new Supplier("Tesla", "The stuff Elon Musk makes when he's bored");
+        supplierDataStore.add(tesla);
 
         //setting up a new product category
         ProductCategory tablet = new ProductCategory("Tablet", "Hardware", "A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display.");
         productCategoryDataStore.add(tablet);
 
-        ProductCategory desktop = new ProductCategory("Desktop", "Hardware", "Test");
+        ProductCategory desktop = new ProductCategory("Desktop", "Hardware", "Weird box with black magic happening inside.");
         productCategoryDataStore.add(desktop);
+
+        ProductCategory smartphone = new ProductCategory("Smartphone", "Hardware", "That think you use all day.");
+        productCategoryDataStore.add(smartphone);
+
+        ProductCategory car = new ProductCategory("Car", "Vehicles", "vroom vroom");
+        productCategoryDataStore.add(car);
 
         //setting up products and printing it
         productDataStore.add(new Product("Amazon Fire", 49.9f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, amazon));
         productDataStore.add(new Product("Lenovo IdeaPad Miix 700", 479, "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", tablet, lenovo));
         productDataStore.add(new Product("Amazon Fire HD 8", 89, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", tablet, amazon));
-
-        productDataStore.add(new Product("Apple Macbook", 50, "USD", "Its a desktop machine", desktop, amazon));
+        productDataStore.add(new Product("Apple Macbook", 1300, "USD", "It's a desktop machine", desktop, apple));
+        productDataStore.add(new Product("Apple Macbook 2", 1899, "USD", "It's a desktop machine but newer. 50% more power efficiency, also uses 50% more power.", desktop, apple));
+        productDataStore.add(new Product("Samsung Galaxy S nagyonsok X Pro 2019", 1000, "USD", "I'm running out of ideas", smartphone, samsung));
+        productDataStore.add(new Product("Self-driving car", 999999999, "USD", "If you're already living in 3019", car, tesla));
     }
 }
