741afd93e5ed72dad1ef842d22fa4e2f55c53cd7
danielKovacs94 <dankovacs94@gmail.com>
2019-09-26 23:47:23 +0200

    paypal payment hotfix

--- src/main/webapp/static/js/payment.js
+++ src/main/webapp/static/js/payment.js
@@ -1,13 +1,25 @@
-let radioButtons = document.querySelectorAll('.form-check-input');
+const radioButtons = document.querySelectorAll('.form-check-input');
+
+const paypalHolder = document.querySelector('#paypal-payment-holder');
+const cardHolder = document.querySelector('#card-payment-holder');
+
+const paypalInputFields = paypalHolder.querySelectorAll("input");
+const cardInputFields = cardHolder.querySelectorAll("input");
 
 for (let radioButton of radioButtons) {
     radioButton.addEventListener("click", function () {
         if (radioButton.id === "paypal" && radioButton.checked === true) {
-            document.querySelector('#paypal-payment-holder').classList.remove('paypal-payment-holder-hide');
-            document.querySelector('#card-payment-holder').classList.add('card-payment-holder-hide');
+            for (let input of cardInputFields){
+                input.required = false;
+            }
+            paypalHolder.classList.remove('paypal-payment-holder-hide');
+            cardHolder.classList.add('card-payment-holder-hide');
         } else if (radioButton.id === "card" && radioButton.checked === true) {
-            document.querySelector('#paypal-payment-holder').classList.add('paypal-payment-holder-hide');
-            document.querySelector('#card-payment-holder').classList.remove('card-payment-holder-hide');
+            for (let input of paypalInputFields){
+                input.required = false;
+            }
+            paypalHolder.classList.add('paypal-payment-holder-hide');
+            cardHolder.classList.remove('card-payment-holder-hide');
         }
     });
 }
