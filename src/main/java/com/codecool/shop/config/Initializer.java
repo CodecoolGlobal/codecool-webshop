e3c129444c92365400072b533da2d69354c39a0b
daniel-kalocsay <kdani782@gmail.com>
2019-10-09 14:42:32 +0200

    sql_initialization fills db tables with records

--- src/main/java/com/codecool/shop/config/Initializer.java
+++ src/main/java/com/codecool/shop/config/Initializer.java
@@ -38,101 +38,101 @@ public class Initializer implements ServletContextListener {
             System.out.println(category);
         }
 
-        /*
-        ProductDao productDataStore = ProductDaoMem.getInstance();
-        ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
-        SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
 
-        //setting up a new supplier
-        Supplier amazon = new Supplier("Amazon", "Digital content and services");
-        supplierDataStore.add(amazon);
-        Supplier lenovo = new Supplier("Lenovo", "Computers");
-        supplierDataStore.add(lenovo);
-        Supplier apple = new Supplier("Apple", "All kinds of useless overpriced hardware");
-        supplierDataStore.add(apple);
-        Supplier samsung = new Supplier("Samsung", "All kinds of actually useful hardware. Still overpriced");
-        supplierDataStore.add(samsung);
-        Supplier tesla = new Supplier("Tesla", "The stuff Elon Musk makes when he's bored");
-        supplierDataStore.add(tesla);
-        Supplier stofi = new Supplier("Stofi", "A phenomenon that nobody will ever really understand.");
-        supplierDataStore.add(stofi);
-
-        //setting up a new product category
-        ProductCategory tablet = new ProductCategory("Tablet", "Hardware", "Thin, flat mobile computer with a touchscreen display");
-        productCategoryDataStore.add(tablet);
-
-        ProductCategory laptop = new ProductCategory("Laptop", "Hardware", "Weird flat boxes with black magic happening inside");
-        productCategoryDataStore.add(laptop);
-
-        ProductCategory smartphone = new ProductCategory("Smartphone", "Hardware", "That thing you use all day");
-        productCategoryDataStore.add(smartphone);
-
-        ProductCategory car = new ProductCategory("Car", "Vehicles", "vroom vroom");
-        productCategoryDataStore.add(car);
-
-        ProductCategory accessory = new ProductCategory("Accessory", "Whatever", "Literally just accessories");
-        productCategoryDataStore.add(accessory);
-
-        //setting up products and printing it
-
-        // product_1
-        productDataStore.add(new Product("Amazon Fire", 49.99f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, amazon));
-
-        // product_2
-        productDataStore.add(new Product("Lenovo IdeaPad Miix 700", 479, "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", tablet, lenovo));
-
-        // product_3
-        productDataStore.add(new Product("Amazon Fire HD 8", 89, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", tablet, amazon));
-
-        // product_4
-        productDataStore.add(new Product("Apple Macbook", 1300, "USD", "It's a desktop machine.", laptop, apple));
-
-        // product_5
-        productDataStore.add(new Product("Apfel Macbuch Luft", 1899, "USD", "It's a desktop machine but newer. 50% more power efficiency, also uses 50% more power. Only 46% more expensive", laptop, apple));
-
-        // product_6
-        productDataStore.add(new Product("Samsung Galaxy S nagyonsok Pro X 2019.v42", 1000, "USD", "I'm running out of ideas.", smartphone, samsung));
-
-        // product_7
-        productDataStore.add(new Product("Self-driving car", 9999999, "USD", "If you're already living in 3019.", car, tesla));
-
-        // product_8
-        productDataStore.add(new Product("Wireless phone charger", 49, "USD", "More oomph for your phone, anywhere!", accessory, tesla));
-
-        // product_9
-        productDataStore.add(new Product("Tesla Shirt", 39, "USD", "Wearing this will double your IQ.", accessory, tesla));
-
-        // product_10
-        productDataStore.add(new Product("I don't even know what this is", 19, "USD", "some screen cleaner i guess", accessory, apple));
-
-        // product_11
-        productDataStore.add(new Product("Death Ray", 0, "USD", "Or the Peace Ray, as Tesla called it. " +
-                "Tesla believed that by accelerating mercury isotopes to 48 times the speed of sound, the resulting " +
-                "beam would produce enough energy to destroy entire armies at a distance limited only by " +
-                "the curvature of the Earth.", accessory, tesla));
-
-        // product_12
-        productDataStore.add(new Product("Zsiguli", 195, "USD", "Some things just never get old.", car,  amazon));
-
-        // product_13
-        productDataStore.add(new Product("Bambuszfogpiszkalo", 0.79f, "USD", "Bamboozled.", accessory, stofi));
-
-        // product_14
-        productDataStore.add(new Product("Tompahegyu nyil", 19.99f, "USD", "Used by the Sami to hunt squirrels.", accessory, stofi));
-
-        // product_15
-        productDataStore.add(new Product("Mr. Bean's Mini", 60000, "USD", "Tiny, cozy, fit for purpose.", car, amazon));
-
-        // product_16
-        productDataStore.add(new Product("iPhone 11", 1099, "USD", "Now with revolutionary fidget spinner camera.", smartphone, apple));
-
-        // product_17
-        productDataStore.add(new Product("iLight 7s", 85, "USD", "For those who want to fit the world in their pockets.", smartphone, amazon));
-
-        // product_18
-        productDataStore.add(new Product("Dreamcom 10", 1800, "USD", "Fully adjustable. Looks ugly.", laptop, amazon));*/
-
-        //TODO csutkapipa
+//        ProductDao productDataStore = ProductDaoMem.getInstance();
+//        ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
+//        SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
+//
+//        //setting up a new supplier
+//        Supplier amazon = new Supplier("Amazon", "Digital content and services");
+//        supplierDataStore.add(amazon);
+//        Supplier lenovo = new Supplier("Lenovo", "Computers");
+//        supplierDataStore.add(lenovo);
+//        Supplier apple = new Supplier("Apple", "All kinds of useless overpriced hardware");
+//        supplierDataStore.add(apple);
+//        Supplier samsung = new Supplier("Samsung", "All kinds of actually useful hardware. Still overpriced");
+//        supplierDataStore.add(samsung);
+//        Supplier tesla = new Supplier("Tesla", "The stuff Elon Musk makes when he's bored");
+//        supplierDataStore.add(tesla);
+//        Supplier stofi = new Supplier("Stofi", "A phenomenon that nobody will ever really understand.");
+//        supplierDataStore.add(stofi);
+//
+//        //setting up a new product category
+//        ProductCategory tablet = new ProductCategory("Tablet", "Hardware", "Thin, flat mobile computer with a touchscreen display");
+//        productCategoryDataStore.add(tablet);
+//
+//        ProductCategory laptop = new ProductCategory("Laptop", "Hardware", "Weird flat boxes with black magic happening inside");
+//        productCategoryDataStore.add(laptop);
+//
+//        ProductCategory smartphone = new ProductCategory("Smartphone", "Hardware", "That thing you use all day");
+//        productCategoryDataStore.add(smartphone);
+//
+//        ProductCategory car = new ProductCategory("Car", "Vehicles", "vroom vroom");
+//        productCategoryDataStore.add(car);
+//
+//        ProductCategory accessory = new ProductCategory("Accessory", "Whatever", "Literally just accessories");
+//        productCategoryDataStore.add(accessory);
+//
+//        //setting up products and printing it
+//
+//        // product_1
+//        productDataStore.add(new Product("Amazon Fire", 49.99f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, amazon));
+//
+//        // product_2
+//        productDataStore.add(new Product("Lenovo IdeaPad Miix 700", 479, "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", tablet, lenovo));
+//
+//        // product_3
+//        productDataStore.add(new Product("Amazon Fire HD 8", 89, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", tablet, amazon));
+//
+//        // product_4
+//        productDataStore.add(new Product("Apple Macbook", 1300, "USD", "It's a desktop machine.", laptop, apple));
+//
+//        // product_5
+//        productDataStore.add(new Product("Apfel Macbuch Luft", 1899, "USD", "It's a desktop machine but newer. 50% more power efficiency, also uses 50% more power. Only 46% more expensive", laptop, apple));
+//
+//        // product_6
+//        productDataStore.add(new Product("Samsung Galaxy S nagyonsok Pro X 2019.v42", 1000, "USD", "I'm running out of ideas.", smartphone, samsung));
+//
+//        // product_7
+//        productDataStore.add(new Product("Self-driving car", 9999999, "USD", "If you're already living in 3019.", car, tesla));
+//
+//        // product_8
+//        productDataStore.add(new Product("Wireless phone charger", 49, "USD", "More oomph for your phone, anywhere!", accessory, tesla));
+//
+//        // product_9
+//        productDataStore.add(new Product("Tesla Shirt", 39, "USD", "Wearing this will double your IQ.", accessory, tesla));
+//
+//        // product_10
+//        productDataStore.add(new Product("I don't even know what this is", 19, "USD", "some screen cleaner i guess", accessory, apple));
+//
+//        // product_11
+//        productDataStore.add(new Product("Death Ray", 0, "USD", "Or the Peace Ray, as Tesla called it. " +
+//                "Tesla believed that by accelerating mercury isotopes to 48 times the speed of sound, the resulting " +
+//                "beam would produce enough energy to destroy entire armies at a distance limited only by " +
+//                "the curvature of the Earth.", accessory, tesla));
+//
+//        // product_12
+//        productDataStore.add(new Product("Zsiguli", 195, "USD", "Some things just never get old.", car,  amazon));
+//
+//        // product_13
+//        productDataStore.add(new Product("Bambuszfogpiszkalo", 0.79f, "USD", "Bamboozled.", accessory, stofi));
+//
+//        // product_14
+//        productDataStore.add(new Product("Tompahegyu nyil", 19.99f, "USD", "Used by the Sami to hunt squirrels.", accessory, stofi));
+//
+//        // product_15
+//        productDataStore.add(new Product("Mr. Bean's Mini", 60000, "USD", "Tiny, cozy, fit for purpose.", car, amazon));
+//
+//        // product_16
+//        productDataStore.add(new Product("iPhone 11", 1099, "USD", "Now with revolutionary fidget spinner camera.", smartphone, apple));
+//
+//        // product_17
+//        productDataStore.add(new Product("iLight 7s", 85, "USD", "For those who want to fit the world in their pockets.", smartphone, amazon));
+//
+//        // product_18
+//        productDataStore.add(new Product("Dreamcom 10", 1800, "USD", "Fully adjustable. Looks ugly.", laptop, amazon));
+//
+//        //TODO csutkapipa
     }
 
     @Override
--- src/main/sql/init_db_sql.sql
+++ src/main/sql/init_db_sql.sql
@@ -1,60 +1,85 @@
-DROP TABLE IF EXISTS orders;
-DROP TABLE IF EXISTS cart;
-DROP TABLE IF EXISTS product;
 DROP TABLE IF EXISTS product_category;
 DROP TABLE IF EXISTS supplier;
+DROP TABLE IF EXISTS product;
+DROP TABLE IF EXISTS cart;
+DROP TABLE IF EXISTS orders;
 
 CREATE TABLE product_category (
-                                  id serial PRIMARY KEY,
-                                  name VARCHAR(30),
-                                  description VARCHAR(150),
-                                  department VARCHAR(30)
+                          id serial PRIMARY KEY,
+                          name VARCHAR,
+                          department VARCHAR,
+                          description VARCHAR
 );
 
 CREATE TABLE supplier (
                           id serial PRIMARY KEY,
-                          name VARCHAR(30),
-                          description VARCHAR(150)
+                          name VARCHAR,
+                          description VARCHAR
 );
 
 CREATE TABLE product (
                          id serial PRIMARY KEY,
-                         name VARCHAR(50),
-                         description VARCHAR(150),
+                         name VARCHAR,
+                         description VARCHAR,
                          default_price FLOAT,
                          default_currency VARCHAR(3),
                          product_category INTEGER REFERENCES product_category(id),
-                         supplier INTEGER REFERENCES supplier(id)
+                         supplier INTEGER REFERENCES supplier(id),
+                         image VARCHAR
 );
 
 CREATE TABLE cart (
-                      cart_id INTEGER,
-                      product_id INTEGER REFERENCES product(id)
+                         cart_id INTEGER,
+                         product_id INTEGER REFERENCES product(id)
 );
 
 CREATE TABLE orders (
     id SERIAL PRIMARY KEY,
-    buyer_name VARCHAR(30) NOT NULL,
+    buyer_name VARCHAR NOT NULL,
     buyer_phone VARCHAR(20) NOT NULL,
-    buyer_email VARCHAR(35) NOT NULL,
-    buyer_shipping_address VARCHAR(50) NOT NULL,
-    buyer_billing_address VARCHAR(50) NOT NULL,
+    buyer_email VARCHAR NOT NULL,
+    buyer_shipping_address VARCHAR NOT NULL,
+    buyer_billing_address VARCHAR NOT NULL,
     cart_id INTEGER REFERENCES cart(cart_id)
 );
 
 INSERT INTO supplier (name, description) VALUES
-    ('Apple', 'All kind of useless overpriced hardware'),
-    ('Samsung', 'All kinds od askomfkaaaaa'),
-    ('Stofi', 'A guyasdkongjadokngkoasngoka')
+    ('Amazon', 'Digital content and services'),                                         /* 1 */
+    ('Lenovo', 'Computers'),                                                            /* 2 */
+    ('Apple', 'All kinds of useless overpriced hardware'),                              /* 3 */
+    ('Samsung', 'All kinds of actually useful hardware. Still overpriced'),             /* 4 */
+    ('Tesla', 'The stuff Elon Musk makes when he''s bored'),                            /* 5 */
+    ('Stofi', 'A phenomenon that nobody will ever really understand')                   /* 6 */
 ;
 
-INSERT INTO product_category (name, description, department) VALUES
-    ('Tablet', 'All kind of useless overpadkognjpkas riced hardware', 'hardware'),
-    ('Laptop', 'All kinds od askomfkaaaaa', 'hardware'),
-    ('Smartphone', 'A guyasdkongjadokngkoasngoka', 'hardware')
+INSERT INTO product_category (name, department, description) VALUES
+    ('Tablet', 'Hardware', 'Thin, flat mobile computer with a touchscreen display'),    /* 1 */
+    ('Laptop', 'Hardware', 'Weird flat boxes with black magic happening inside'),       /* 2 */
+    ('Smartphone', 'Hardware', 'That thing you use all day'),                           /* 3 */
+    ('Car', 'Vehicles', 'vroom vroom'),                                                 /* 4 */
+    ('Accessory', 'Whatever', 'Literally just accessories')                             /* 5 */
 ;
 
-INSERT INTO product (name, description, default_price, default_currency, product_category, supplier) VALUES
-    ('Iphone 100 XS MAX PLUS extra', 'All kind of useless overpadkognjpkas riced hardware', 1000, 'USD', 3, 1),
-    ('Mekbúk', 'All kinds od askomfkaaaaa', 100, 'USD', 2, 1)
+INSERT INTO product (name, default_price, default_currency, description, product_category, supplier) VALUES
+    ('Amazon Fire', 49.99, 'USD', 'Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.', 1, 1),
+    ('Lenovo IdeaPad Miix 700', 479, 'USD', 'Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.', 1, 2),
+    ('Amazon Fire HD 8', 89, 'USD', 'Amazon''s latest Fire HD 8 tablet is a great value for media consumption.', 1, 1),
+    ('Apple Macbook', 1300, 'USD', 'It''s a desktop machine.', 2, 3),
+    ('Apfel Macbuch Luft', 1899, 'USD', 'It''s a desktop machine but newer. 50% more power efficiency, also uses 50% more power. Only 46% more expensive', 2, 3),
+    ('Samsung Galaxy S nagyonsok Pro X 2019.v42', 1000, 'USD', 'I''m running out of ideas.', 3, 4),
+    ('Self-driving car', 9999999, 'USD', 'If you''re already living in 3019.', 4, 5),
+    ('Wireless phone charger', 49, 'USD', 'More oomph for your phone, anywhere!', 5, 5),
+    ('Tesla Shirt', 39, 'USD', 'Wearing this will double your IQ.', 5, 5),
+    ('I don''t even know what this is', 19, 'USD', 'some screen cleaner i guess', 5, 3),
+    ('Death Ray', 0, 'USD', 'Or the Peace Ray, as Tesla called it.' ||
+                            'Tesla believed that by accelerating mercury isotopes to 48 times the speed of sound,' ||
+                            'the resulting beam would produce enough energy to destroy entire armies at a distance' ||
+                            'limited only by the curvature of the Earth.', 5, 5),
+    ('Zsiguli', 195, 'USD', 'Some things just never get old.', 4,  1),
+    ('Bambuszfogpiszkáló', 0.79, 'USD', 'Bamboozled.', 5, 6),
+    ('Tompahegyű nyíl', 19.99, 'USD', 'Used by the Sami to hunt squirrels.', 5, 6),
+    ('"Mr. Bean''s Mini', 60000, 'USD', 'Tiny, cozy, fit for purpose.', 4, 1),
+    ('"iPhone 11', 1099, 'USD', 'Now with revolutionary fidget spinner camera.', 3, 3),
+    ('iLight 7s', 85, 'USD', 'For those who want to fit the world in their pockets.', 3, 1),
+    ('Dreamcom 10', 1800, 'USD', 'Fully adjustable. Looks ugly.', 2, 1)
 ;
