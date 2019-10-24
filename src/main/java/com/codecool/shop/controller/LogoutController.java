eb1be4233d40732bafe3d7ff173e6d321011196a
danielKovacs94 <dankovacs94@gmail.com>
2019-10-24 15:20:47 +0200

    implement logout

---
+++ src/main/java/com/codecool/shop/controller/LogoutController.java
@@ -0,0 +1,26 @@
+package com.codecool.shop.controller;
+
+import com.codecool.shop.dao.implementation.Connector;
+import com.codecool.shop.dao.implementation.UserDaoJDBC;
+import org.mindrot.jbcrypt.BCrypt;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.HashMap;
+
+@WebServlet(urlPatterns = {"/logout"})
+public class LogoutController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        session.removeAttribute("user_name");
+        session.removeAttribute("user_id");
+        resp.sendRedirect("/");
+    }
+}
